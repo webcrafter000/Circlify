311b6859f5b823b1b5828d40026713bd167658ff
Nag <nalamnagendra1611@gmail.com>
2024-11-27 20:31:25 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
+        sm: "640px", // Small screens (mobile)
+        md: "768px", // Medium screens (tablets)
+        lg: "1024px", // Large screens (desktops)
       },
     },
     extend: {
